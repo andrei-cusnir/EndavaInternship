df02724acbfb97aea96c522ef46db025e7efd409
Andrei A Cusnir <andcusnir@endava.com>
2019-09-25 18:22:26 +0300

    ...

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         StudentSet <Student> students = new StudentSet<>();
-
+        Student vasea = new Student("Vasea",11.11.11,"details");
+        students.add(vasea);
+        System.out.println(vasea);
     }
 
 
